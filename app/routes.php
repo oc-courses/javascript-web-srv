f3290f0384fbf96468c3a18d4cfd9bc4be2a8d3b
bpesquet <bpesquet@gmail.com>
2016-05-05 12:44:46 +0200

    Add lexicon route

--- app/routes.php
+++ app/routes.php
@@ -27,6 +27,39 @@ $app->post('/api/article', function(Request $request) use ($app) {
     return $app->json($articleId, 201);  // 201 = Created
 });
 
+// Return words starting with a particular letter in JSON format
+// Handles only "A", "B" and "C" letters
+$app->get('/api/lexique/{letter}', function ($letter, Request $request) use ($app) {
+    $words = array();
+    switch($letter) {
+        case "A":
+        array_push($words, array(
+            'term' => 'Acronyme',
+            'definition' => 'Sigle qui se prononce comme un mot ordinaire, sans épeler les lettres'));
+        array_push($words, array(
+            'term' => 'Asymptote',
+            'definition' => 'Droite, cercle ou point dont une courbe plus complexe peut se rapprocher'));
+        break;
+        case "B":
+        array_push($words, array(
+            'term' => 'Biopsie',
+            'definition' => "Prélèvement d'une très petite partie d'un organe ou d'un tissu pour effectuer des examens"));
+        array_push($words, array(
+            'term' => 'Botulisme',
+            'definition' => 'Maladie paralytique rare mais grave due à une neurotoxine bactérienne, la toxine botulique'));
+        break;
+        case "C":
+        array_push($words, array(
+            'term' => 'Cacochyme',
+            'definition' => "En état d’extrême faiblesse due à la vieillesse"));
+        array_push($words, array(
+            'term' => 'Cuticule',
+            'definition' => "Couche externe qui recouvre et protège les organes aériens des végétaux et les organes de certains animaux"));
+        break;
+    }
+    return $app->json($words);
+});
+
 $app->get('/api/liens', function() use ($app) {
     $sql = "select l.link_id as id, link_title as titre, link_url as url, link_author as auteur, count(com_id) as commentaires
         from link l left join comment c on l.link_id=c.link_id group by l.link_id order by l.link_id desc";
