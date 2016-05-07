6690a87075f565a4a34b4f24e21e5b8a60cd8adf
bpesquet <bpesquet@gmail.com>
2016-05-07 15:35:48 +0200

    Reorder services

--- app/app.php
+++ app/app.php
@@ -18,22 +18,24 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../views',
 ));
 $app->register(new Silex\Provider\UrlGeneratorServiceProvider());
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
+/*$app->register(new Silex\Provider\MonologServiceProvider(), array(
     'monolog.logfile' => __DIR__.'/../jswebsrv.log',
     'monolog.name' => 'JSWEBSSRV',
     'monolog.level' => 'INFO'
-));
+));*/
+
+// Allow cross-domain requests
 $app->register(new JDesrosiers\Silex\Provider\CorsServiceProvider());
+$app->after($app["cors"]);
 
 // Register JSON data decoder for JSON requests
 $app->before(function (Request $request) {
     if (0 === strpos($request->headers->get('Content-Type'), 'application/json')) {
+        // Define JSON payload as request parameters
         $data = json_decode($request->getContent(), true);
         $request->request->replace(is_array($data) ? $data : array());
     }
 });
-
-$app->after($app["cors"]);
 
 // enable the debug mode
 $app['debug'] = true;
