6dbd0c896afe4048305c2cc4d35487d96fb64429
Dany <danyweismail@gmail.com>
2017-04-30 22:22:56 +0200

    ajout des header
    Je sais pas pourquoi mais je suis sur Linux et j'ai donné toutes les permissions à tous les fichiers mais sans le header("Access-Control-Allow-Origin: *");  je recevais en permanence des erreurs

--- post_json.php
+++ post_json.php
@@ -1,3 +1,5 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 $data = print_r(json_decode(file_get_contents('php://input'), true), true);
 file_put_contents("post_json.log", $data);
